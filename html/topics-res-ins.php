1a9c307e9eb4d6c8cb7f185aaee2a424092cc013
Ktento <kento07210525mo@outlook.jp>
2025-01-24 09:43:08 +0900

    トピックへコメント登録

---
+++ html/topics-res-ins.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>トピックへコメント登録</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <?php include 'templates/header.php'; ?>
+    <div class="container">
+        <form>
+            <label for="registration-number">登録番号</label>
+            <input type="text" id="registration-number" name="registration-number" value="" disabled><br>
+
+            <label for="title">タイトル</label>
+            <input type="text" id="title" name="title" value="" disabled><br>
+
+            <label for="category">種類</label><br>
+            <input type="radio" id="感想" name="category" value="感想" required>
+            <label for="感想">感想</label><br>
+            <input type="radio" id="質問" name="category" value="質問" required>
+            <label for="質問">質問</label><br>
+
+            <label for="comment">コメント</label><br>
+            <textarea id="comment" name="comment" rows="5" required></textarea><br>
+
+            <div class="buttons">
+                <button type="submit" id="insert" name="action" value="insert">更新</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
+
+<?php
+require_once 'includes/auth.php';
+?>
+<!-- comment upd.php -->
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    //SQL文
+    $sql = 'UPDATE topic_comment SET topic_comment=:topic_comment WHERE topic_comment_id=:topic_comment_id';
+    //DBへの接続
+    $dsn = 'mysql:host=localhost;dbname=artifact;charset=utf8';
+    $user = "user01";
+    $pass = "user01";
+    try {
+        //SQLの実行
+        $topic_comment_id = $_POST['registration-number'];
+        $topic_comment = $_POST['*comment'];
+        $pdo = new PDO($dsn, $user, $pass);
+        //SQLの実行
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':topic_comment_id', $topic_comment_id);
+        $stmt->bindValue(':topic_comment', $topic_comment);
+        if ($stmt->execute()) {
+            echo "更新成功";
+        } else {
+            echo $result;
+            echo "更新失敗";
+        }
+        // foreachの値を変数に格納したい
+    } catch (PDOException $e) {
+        echo "接続失敗: " . $e->getMessage() . "\n";
+    } finally {
+        // DB接続を閉じる
+        $pdo = null;
+    }
+}
+?>
