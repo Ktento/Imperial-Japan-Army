8fee1613e8870fbcd639b841ea76efd2d5eb06ac
Ktento <kento07210525mo@outlook.jp>
2025-01-24 11:30:07 +0900

    再表示する処理を追加

--- html/topics-res-ins.php
+++ html/topics-res-ins.php
@@ -6,6 +6,13 @@ $topic_id = sanitizeInput($_GET['i'] ?? '');
 $title = sanitizeInput($_GET['t'] ?? '');
 $category = sanitizeInput($_GET['c'] ?? '');
 $target = sanitizeInput($_GET['a'] ?? '');
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $topic_id = $_POST['comment_id'] ?? $topic_id;
+    $title = $_POST['title'] ?? $title;
+    $category = $_POST['category'] ?? $category;
+    $target = $_POST['target'] ?? $target;
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -38,7 +45,7 @@ $target = sanitizeInput($_GET['a'] ?? '');
                             <label for="" class="">タイトル:</label>
                         </dt>
                         <dd class="ml-64">
-                            <?= $title ?>
+                            <?= htmlspecialchars($title) ?>
                         </dd>
                     </dl>
                     <dl class="py-2">
@@ -46,7 +53,7 @@ $target = sanitizeInput($_GET['a'] ?? '');
                             <label for="" class="">種類:</label>
                         </dt>
                         <dd class="ml-64">
-                            <?= $category ?>
+                            <?= htmlspecialchars($category) ?>
                         </dd>
                     </dl>
                     <dl class="py-2">
@@ -54,7 +61,7 @@ $target = sanitizeInput($_GET['a'] ?? '');
                             <label for="" class="">対象:</label>
                         </dt>
                         <dd class="ml-64">
-                            <?= $target ?>
+                            <?= htmlspecialchars($target) ?>
                         </dd>
                     </dl>
                     <dl class="py-2">
