92a41f32f70d4e0b61e4c72e01a525d1b2cc676f
san-j23005 <sangi-j23005@sangi.jp>
2025-01-24 11:53:31 +0900

    fix #7 編集ボタンを正常に動作するように

--- html/views/comments.php
+++ html/views/comments.php
@@ -1,5 +1,5 @@
 <?php foreach ($comments as $comment):
-    $comment_id
+    $comment_id = htmlspecialchars($comment['comment_id'] ?? "0", ENT_QUOTES, 'UTF-8');
     $name = htmlspecialchars($comment['user_name'] ?? "取得できませんでした", ENT_QUOTES, 'UTF-8');
     $content = htmlspecialchars($comment['comment'] ?? "取得できませんでした", ENT_QUOTES, 'UTF-8');
     $created_at = htmlspecialchars(formatJapaneseDate($comment['created_at']) ?? "取得できませんでした", ENT_QUOTES, 'UTF-8');
@@ -21,8 +21,8 @@
                 <p class="mt-2 text-gray-800"><?= $content ?></p>
             </div>
             <div class="flex space-x-2">
-                <a class="p-1 text-xs border border-gray-300 hover:bg-gray-200" href="topics-res-upd.php?topic_id=<?= $topic_id ?>">編集</a>
-                <a class="p-1 text-xs border border-red-300 text-red-600 hover:bg-red-200" href="topics-res-del.php?topic_id=<?= $topic_id ?>">削除</a>
+                <a class="p-1 text-xs border border-gray-300 hover:bg-gray-200" href="topics-res-upd.php?i=<?= $comment_id ?>&t=<?= $title ?>&c=<?= $category ?>&a=<?= $target ?>">編集</a>
+                <a class="p-1 text-xs border border-red-300 text-red-600 hover:bg-red-200" href="topics-res-del.php?i=<?= $comment_id ?>&t=<?= $title ?>&c=<?= $category ?>&a=<?= $target ?>">削除</a>
             </div>
         </div>
         <div class="w-1/4 text-left text-xs text-gray-600 space-y-1 pl-6">
