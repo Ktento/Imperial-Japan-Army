9aaf8fc74f01b13ebac310b2309c417213a59107
san-j23005 <san-j23005@sangi.jp>
2025-01-28 19:41:42 +0900

    feat navibar

--- html/templates/header.php
+++ html/templates/header.php
@@ -1,13 +1,14 @@
-<header class="w-full bg-slate-800 text-white py-4">
-    <div class="container mx-auto flex justify-between items-center px-4">
+<header class="w-full bg-slate-800 text-white">
+    <div class="container mx-auto flex justify-between items-center px-4 py-1">
         <a class="text-xl font-bold" href="top.php">大日本帝国陸軍</a>
         <p class="text-sm">こんにちは、<?= htmlspecialchars($username ?? 'ゲスト', ENT_QUOTES, 'UTF-8') ?> さん</p>
     </div>
-    <div class="container mx-auto flex items-center px-4 gap-2">
-        <a href="login.php" class="px-4 py-2 text-white text-sm">トップ</a>
-        <a href="login.php" class="px-4 py-2 text-white text-sm">トピック登録</a>
-        <a href="user-ins.php" class="px-4 py-2 hover:bg-slate-900 rounded text-white text-sm">メディア登録</a>
-        <a href="user-ins.php" class="px-4 py-2 hover:bg-slate-900 rounded text-white text-sm">ユーザ登録</a>
-        <a href="user-ins.php" class="px-4 py-2 hover:bg-slate-900 rounded text-white text-sm">ユーザ一覧</a>
+    <div class="container mx-auto flex items-center px-4 pb-2 gap-2">
+        <a href="top.php" class="px-4 py-1 hover:bg-slate-700 rounded text-white text-sm">トップ</a>
+        <a href="login.php" class="px-4 py-1 hover:bg-slate-700 rounded text-white text-sm">ログイン</a>
+        <a href="topics-ins.php" class="px-4 py-1 hover:bg-slate-700 rounded text-white text-sm">トピック登録</a>
+        <a href="media-ins.php" class="px-4 py-1 hover:bg-slate-700 rounded text-white text-sm">メディア登録</a>
+        <a href="user-ins.php" class="px-4 py-1 hover:bg-slate-700 rounded text-white text-sm">ユーザ登録</a>
+        <a href="user-list.php" class="px-4 py-1 hover:bg-slate-700 rounded text-white text-sm">ユーザ一覧</a>
     </div>
 </header>
