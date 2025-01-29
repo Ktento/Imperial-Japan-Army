5dab43a0eb5a18567681b19020f0b42dcabe5003
san-j23005 <san-j23005@sangi.jp>
2025-01-30 06:51:30 +0900

    詳細からタグを押したら、フィルタリングされたトップへ

--- html/views/tags.php
+++ html/views/tags.php
@@ -1,4 +1,5 @@
 
+
 <?php foreach ($tags as $tag): ?>
-    <span class="px-3 py-1 border text-xs"><?= htmlspecialchars($tag, ENT_QUOTES, 'UTF-8') ?></span>
+    <a href="top.php?tags=<?= $tag ?>" class="px-3 py-1 border text-xs hover:text-blue-500"><?= htmlspecialchars($tag, ENT_QUOTES, 'UTF-8') ?></a>
 <?php endforeach; ?>
