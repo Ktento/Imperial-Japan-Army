8cfb5892c5660363c1db2b6f3c2bb3ae5e7a9976
san-j23005 <san-j23005@sangi.jp>
2025-01-30 06:42:05 +0900

    ref media詳細 -> メディア詳細

--- html/media-dtl.php
+++ html/media-dtl.php
@@ -32,7 +32,7 @@ require_once 'test/tests.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>media詳細</title>
+    <title>メディア詳細</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
